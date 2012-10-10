4b0001e23761a76f39d52abcb76e306d9393a620
No9 <antonwhalley@vodafone.ie>
2012-10-10 21:03:44 +0100

    Parsing file complete

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,14 @@
 var assert = require('assert');
 
+function TEST (name){
+	
+}
+
+TEST.method = function (text){
+	console.log(text);
+	return "returned";
+}
+
+var retval = TEST.method("OUTPUT");
+console.log(retval);
 assert.equal(true, true);
